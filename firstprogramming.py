b4162d41aea0a7fdca7c28c11c2c6b116469f79c
MuhammadAffan9 <muhammadaffan4420@gmail.com>
2025-05-05 17:56:38 +0500

    Add files via upload

---
+++ firstprogramming.py
@@ -0,0 +1,1 @@
+print ("Hello World !. My name is Muhammad Affan and my student ID is 0022") # This programme prints my name and student ID and a greeting message
---
+++ secondprogramming copy.py
@@ -0,0 +1,10 @@
+firstname="Affan"
+secondname="Ali"
+name1="Affan Ali"
+FirstName="Muhammad"
+SecondName="Affan Ali"
+print (SecondName)
+print (firstname)
+print (name1)
+print (FirstName)
+print (secondname)
