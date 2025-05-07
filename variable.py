2cdd806dfec926f628da272ce443156d7a7f73f4
MuhammadAffan9 <muhammadaffan4420@gmail.com>
2025-05-07 16:26:22 +0500

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,11 @@
+
+first_name="Affan"
+second_name="Ali"
+name1="Affan Ali"
+FirstName="Muhammad"
+SecondName="Affan Ali"
+print (SecondName)
+print (first_name)
+print (name1)
+print (FirstName)
+print (second_name)
