c28cfd921d1b937bf7321921348105ed3d719408
MuhammadAffan9 <muhammadaffan4420@gmail.com>
2025-05-08 15:05:54 +0500

    Add files via upload

---
+++ arethmatic_operators.py
@@ -0,0 +1,17 @@
+a = 50
+b = 3
+print("a + b = ", a + b)  # This will add a and b
+print("a - b = ", a - b)  # This will subtract b from a
+print("a * b = ", a * b)  # This will multiply a and b
+print("a / b = ", a / b)  # This will divide a by b
+print("a // b = ", a // b)  # This will perform floor division of a by b
+print("a % b = ", a % b)  # This will give the remainder of a divided by b
+print("a ** b = ", a ** b)  # This will raise a to the power of b
+
+print(f"50 + 3 = {a + b}")
+print(f"50 - 3 = {a - b}")
+print(f"50 * 3 = {a * b}")
+print(f"50 / 3 = {a / b}")
+print(f"50 // 3 = {a // b}")
+print(f"50 % 3 = {a % b}")
+print(f"50 ** 3 = {a ** b}")
