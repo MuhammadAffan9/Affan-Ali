77347fa5991c76bf2fbaefb82ab620099346efef
MuhammadAffan9 <muhammadaffan4420@gmail.com>
2025-05-08 14:52:48 +0500

    Add files via upload

---
+++ strings_variables.py
@@ -0,0 +1,25 @@
+place1 = 'Abottabad' 
+place2 = 'Islamabad' 
+place3 = 'Lahore' 
+print(f"{place1}, {place2}, {place3}")  # This programme prints the names of three cities
+
+my_favoutite_animal = "Wolf"
+speciality = "Intelligence, loyalty, and strength"
+print(f"{my_favoutite_animal} is my favourite animal because of its {speciality}.")  # This programme prints my favourite animal and the reason why I like it
+
+first_name = "Muhammad Affan"
+last_name = "Ali"
+full_name = first_name + " " + last_name
+print(full_name)
+
+say = "Hi "
+print(say * 10)
+
+personality = "PythonRocks"
+print(personality[0])
+print(personality[3])
+print(personality[10])
+
+what_i_like = "I like Python programming"
+print(what_i_like[2:6])
+print(what_i_like[7:13])
