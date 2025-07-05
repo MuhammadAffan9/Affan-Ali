22b47c422aab21d0ce0d5632db07652435049b0e
MuhammadAffan9 <muhammadaffan4420@gmail.com>
2025-07-05 16:21:46 +0500

    conditional_assignment.py

--- conditional_assignment.py
+++ conditional_assignment.py
@@ -1,41 +1,39 @@
-print("Hi! I'm Robo, your fun robot friend. Let's play with choices!")
+print("Hi! I'm BRAINY 1.0 i am here to  play with u")
 
-# Check age
-print("\nHow old are you?")
+print("\nHow old are you?") #type your age
 age = int(input("Type your age: "))
 if age >= 18:
     print("Wow, you're an adult! You can do big things!")
-else:
+else:                          # tells u about your age
     print("You're a kid like me! Kids are super cool!")
 
-# Grade a test
-print("\nWhat's your test score?")
+print("\nWhat's your test score?")   #type your score
 marks = int(input("Type your score: "))
 if marks >= 90:
     print("Grade: A - You're a star!")
 elif marks >= 70:
     print("Grade: B - Awesome job!")
-elif marks >= 50:
+elif marks >= 50:                            #tell you about your score
     print("Grade: C - Keep trying!")
-else:
+else:                                                     
     print("Fail - Don't worry, you'll get it next time!")
 
-# Weather tip
-print("\nWhat's the weather today?")
+
+print("\nWhat's the weather today?") #type weather
 weather = input("Type rainy, sunny, or cloudy: ")
-if weather.lower() == "rainy":
+if weather.lower() == "rainy":                        
     print("Grab an umbrella!")
 elif weather.lower() == "sunny":
-    print("Put on sunglasses!")
+    print("Put on sunglasses!")         #pass comment about weather
 elif weather.lower() == "cloudy":
     print("Maybe bring a jacket!")
 else:
     print("Stay ready for anything!")
 
-# Hungry check
+
 print("\nAre you hungry?")
 hungry = input("Type yes or no: ")
-if hungry.lower() == "yes":
+if hungry.lower() == "yes":                 #type yes or no and you will get order to eat 
     print("Eat something tasty!")
 else:
     print("Snack time later then!")
@@ -43,13 +41,13 @@ else:
 # Mood chat
 print("\nHow do you feel?")
 mood = input("Type happy, sad, or bored: ")
-if mood.lower() == "happy":
+if mood.lower() == "happy":                      #type how u feel and you will be preaised by him
     print("Yay, keep shining!")
 elif mood.lower() == "sad":
     print("Cheer up soon, okay?")
 elif mood.lower() == "bored":
     print("Let’s find fun stuff to do!")
 else:
-    print("Thanks for telling me!")
+    print("Thanks for telling me!")                #says goof bye
 
-print("\nBye-bye! Robo had fun with you!")
+print("\nKhuda hafiz i had fun with u")
